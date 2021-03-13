4a869c2378f454776c1714edc7df176894117323
kazuki_moriyama <moriyama0626kazuki@gmail.com>
2021-03-13 10:14:36 +0900

    push

---
+++ SpringSample2-1/src/main/java/com/example/demo/SpringSample21Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSample21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSample21Application.class, args);
+	}
+
+}
